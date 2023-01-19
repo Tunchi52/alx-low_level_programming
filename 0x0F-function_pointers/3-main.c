8a4f6f2968efc722ddd439bb2da25b2f7873e251
ogbuleke <wilfredmmaduabuchi52@gmail.com>
2023-01-18 20:56:16 -0800

    calc program

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,4 +19,39 @@
   * @argc: argument count
   * @argv: argument vector
   *
-  * Return:
+  * Return: 0-on success
+  */
+int main(int argc, char *argv)
+{
+	int num1, num2;
+	char *operator;
+
+	/* Erro if argument are not up to 4 with the executable */
+	if (arc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]); /* if argument is string convert to number*/
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	/* Error if operator is not null or a 2d array*/
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* Error if user tries to divide (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2))
+
+		return (0);
+}
