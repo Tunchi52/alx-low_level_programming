ab9710b27384af2ca0247f21962a74fa4533c6e5
ogbuleke <wilfredmmaduabuchi52@gmail.com>
2023-02-05 07:22:35 -0800

    fixed bug

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	set - 1 << index;
+	set = 1 << index;
 	*n = *n | set;
 	return (1);
 }
